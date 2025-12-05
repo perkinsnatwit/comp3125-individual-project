225cedc7400b2a958cda0f6492fbadb785f7d393
nateperk05@gmail.com <perkinsn@wit.edu>
2025-12-05 15:37:12 -0500

    Corrected which csv gets loaded

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from src.loader import load_data, train_model
 def main():
     """Main entry point for the car price prediction project."""
     model = CarPricePredictor(input_features = 10)
-    X_train, X_test, y_train, y_test = load_data('data/car_price_dataset_cleaned.csv')
+    X_train, X_test, y_train, y_test = load_data('data/car_price_dataset_medium.csv')
     train_model(X_train, y_train, X_test, y_test, model)
 
 if __name__ == '__main__':
