4ce180d153502407bacb72b3832c1ebbea6dc5a1
nateperk05@gmail.com <perkinsn@wit.edu>
2025-12-05 18:24:59 -0500

    Correctly calculates number of input features needed to pass into model

--- src/main.py
+++ src/main.py
@@ -5,8 +5,12 @@ from src.loader import load_data, train_model
 
 def main():
     """Main entry point for the car price prediction project."""
-    model = CarPricePredictor(input_features = 10)
     X_train, X_test, y_train, y_test = load_data('data/car_price_dataset_medium.csv')
+
+    input_features = X_train.shape[1]
+
+    model = CarPricePredictor(input_features=input_features)   
+
     train_model(X_train, y_train, X_test, y_test, model)
 
 if __name__ == '__main__':
