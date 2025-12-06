2630d1e0752650f4f43ec883f96f3c5db7ea7f88
nateperk05@gmail.com <perkinsn@wit.edu>
2025-12-05 22:24:46 -0500

    Added additional layer with bigger sizes

--- src/model.py
+++ src/model.py
@@ -4,12 +4,14 @@ import torch.nn.functional as F
 
 class CarPricePredictor(nn.Module):
     
-    def __init__(self, input_features, hidden_size=128):
+    def __init__(self, input_features, hidden_size=128, dropout_prob=0.3):
         super(CarPricePredictor, self).__init__()
         self.fc1 = nn.Linear(input_features, hidden_size)
-        self.dropout1 = nn.Dropout(0.3)
-        self.fc2 = nn.Linear(hidden_size, hidden_size // 2)
-        self.dropout2 = nn.Dropout(0.3)
+        self.dropout1 = nn.Dropout(dropout_prob)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.dropout2 = nn.Dropout(dropout_prob)
+        self.fc3 = nn.Linear(hidden_size, hidden_size // 2)
+        self.dropout3 = nn.Dropout(dropout_prob)
         self.out = nn.Linear(hidden_size // 2, 1)
     
     def forward(self, x):
@@ -17,5 +19,7 @@ class CarPricePredictor(nn.Module):
         x = self.dropout1(x)
         x = F.relu(self.fc2(x))
         x = self.dropout2(x)
+        x = F.relu(self.fc3(x))
+        x = self.dropout3(x)
         x = self.out(x)
         return x
