f02b4c4890ace07c63363bbc08831d70d811554a
nateperk05@gmail.com <perkinsn@wit.edu>
2025-12-05 21:33:09 -0500

    Attempting to dropout 30% of nodes to prevent overfitting during training

--- src/model.py
+++ src/model.py
@@ -7,11 +7,15 @@ class CarPricePredictor(nn.Module):
     def __init__(self, input_features, hidden_size=128):
         super(CarPricePredictor, self).__init__()
         self.fc1 = nn.Linear(input_features, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, hidden_size)
-        self.out = nn.Linear(hidden_size, 1)
+        self.dropout1 = nn.Dropout(0.3)
+        self.fc2 = nn.Linear(hidden_size, hidden_size // 2)
+        self.dropout2 = nn.Dropout(0.3)
+        self.out = nn.Linear(hidden_size // 2, 1)
     
     def forward(self, x):
         x = F.relu(self.fc1(x))
+        x = self.dropout1(x)
         x = F.relu(self.fc2(x))
+        x = self.dropout2(x)
         x = self.out(x)
         return x
